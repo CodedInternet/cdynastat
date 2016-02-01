3fbd90d568495fef35f4c34250dbc8ff45d17a50
Tom Price <tom@codedinternet.com>
2016-02-01 21:08:09 +0000

    Namespace PeerConnectionClient to dynastat

--- src/PeerConnectionClient.cpp
+++ src/PeerConnectionClient.cpp
@@ -4,186 +4,189 @@
 
 #include "PeerConnectionClient.h"
 
-PeerConnectionClient::PeerConnectionClient() {
-    m_endpoint.clear_access_channels(websocketpp::log::alevel::all);
-    m_endpoint.clear_error_channels(websocketpp::log::elevel::all);
+namespace dynastat {
+    PeerConnectionClient::PeerConnectionClient() {
+        m_endpoint.clear_access_channels(websocketpp::log::alevel::all);
+        m_endpoint.clear_error_channels(websocketpp::log::elevel::all);
 
-    m_endpoint.init_asio();
-    m_endpoint.start_perpetual();
+        m_endpoint.init_asio();
+        m_endpoint.start_perpetual();
 
-    m_endpoint.set_tls_init_handler(websocketpp::lib::bind(
-            &PeerConnectionClient::on_tls_init,
-            this,
-            websocketpp::lib::placeholders::_1
-    ));
+        m_endpoint.set_tls_init_handler(websocketpp::lib::bind(
+                &PeerConnectionClient::on_tls_init,
+                this,
+                websocketpp::lib::placeholders::_1
+        ));
 
-    m_thread = websocketpp::lib::make_shared<websocketpp::lib::thread>(&ssl_client::run, &m_endpoint);
-}
-
-PeerConnectionClient::~PeerConnectionClient() {
-    m_endpoint.stop_perpetual();
-}
-
-int PeerConnectionClient::connect(std::string const &uri) {
-    websocketpp::lib::error_code ec;
-
-    ssl_client::connection_ptr con = m_endpoint.get_connection(uri, ec);
-
-    if (ec) {
-        std::cerr << "> Connection initialization error: " << ec.message() << std::endl;
-        return -1;
+        m_thread = websocketpp::lib::make_shared<websocketpp::lib::thread>(&ssl_client::run, &m_endpoint);
     }
 
-    int new_id = m_next_id++;
-    ConnectionMetadata::ptr metadata_ptr = websocketpp::lib::make_shared<ConnectionMetadata>(new_id, con->get_handle(),
-                                                                                             uri);
-    m_connection_list[new_id] = metadata_ptr;
-
-    con->set_open_handler(websocketpp::lib::bind(
-            &ConnectionMetadata::on_open,
-            metadata_ptr,
-            &m_endpoint,
-            websocketpp::lib::placeholders::_1
-    ));
-    con->set_fail_handler(websocketpp::lib::bind(
-            &ConnectionMetadata::on_fail,
-            metadata_ptr,
-            &m_endpoint,
-            websocketpp::lib::placeholders::_1
-    ));
-    con->set_close_handler(websocketpp::lib::bind(
-            &ConnectionMetadata::on_close,
-            metadata_ptr,
-            &m_endpoint,
-            websocketpp::lib::placeholders::_1
-    ));
-    con->set_message_handler(websocketpp::lib::bind(
-            &ConnectionMetadata::on_message,
-            metadata_ptr,
-            websocketpp::lib::placeholders::_1,
-            websocketpp::lib::placeholders::_2
-    ));
-
-    m_endpoint.connect(con);
-
-    return new_id;
-}
-
-void PeerConnectionClient::close(int id, websocketpp::close::status::value code, std::string reason) {
-    websocketpp::lib::error_code ec;
-
-    con_list::iterator metadata_it = m_connection_list.find(id);
-    if (metadata_it == m_connection_list.end()) {
-        std::cerr << "> No connection found with id " << id << std::endl;
-        return;
+    PeerConnectionClient::~PeerConnectionClient() {
+        m_endpoint.stop_perpetual();
     }
 
-    m_endpoint.close(metadata_it->second->get_hdl(), code, reason, ec);
-    if (ec) {
-        std::cerr << "> Error initiating close: " << ec.message() << std::endl;
-    }
-}
+    int PeerConnectionClient::connect(std::string const &uri) {
+        websocketpp::lib::error_code ec;
 
-void ConnectionMetadata::on_open(ssl_client *c, websocketpp::connection_hdl hdl) {
-    m_status = OPEN;
+        ssl_client::connection_ptr con = m_endpoint.get_connection(uri, ec);
 
-    ssl_client::connection_ptr con = c->get_con_from_hdl(hdl);
-    m_server = con->get_response_header("Server");
-}
+        if (ec) {
+            std::cerr << "> Connection initialization error: " << ec.message() << std::endl;
+            return -1;
+        }
 
-void ConnectionMetadata::on_fail(ssl_client *c, websocketpp::connection_hdl hdl) {
-    m_status = FAILED;
+        int new_id = m_next_id++;
+        ConnectionMetadata::ptr metadata_ptr = websocketpp::lib::make_shared<ConnectionMetadata>(new_id,
+                                                                                                 con->get_handle(),
+                                                                                                 uri);
+        m_connection_list[new_id] = metadata_ptr;
 
-    ssl_client::connection_ptr con = c->get_con_from_hdl(hdl);
-    m_server = con->get_response_header("Server");
-    m_error_reason = con->get_ec().message();
-    std::cerr << "> Failed: " << m_error_reason << std::endl;
-}
+        con->set_open_handler(websocketpp::lib::bind(
+                &ConnectionMetadata::on_open,
+                metadata_ptr,
+                &m_endpoint,
+                websocketpp::lib::placeholders::_1
+        ));
+        con->set_fail_handler(websocketpp::lib::bind(
+                &ConnectionMetadata::on_fail,
+                metadata_ptr,
+                &m_endpoint,
+                websocketpp::lib::placeholders::_1
+        ));
+        con->set_close_handler(websocketpp::lib::bind(
+                &ConnectionMetadata::on_close,
+                metadata_ptr,
+                &m_endpoint,
+                websocketpp::lib::placeholders::_1
+        ));
+        con->set_message_handler(websocketpp::lib::bind(
+                &ConnectionMetadata::on_message,
+                metadata_ptr,
+                websocketpp::lib::placeholders::_1,
+                websocketpp::lib::placeholders::_2
+        ));
 
-void ConnectionMetadata::on_close(ssl_client *c, websocketpp::connection_hdl hdl) {
-    m_status = CLOSED;
+        m_endpoint.connect(con);
 
-    ssl_client::connection_ptr con = c->get_con_from_hdl(hdl);
-    std::stringstream s;
-    s << "close code: " << con->get_remote_close_code() << " ("
-    << websocketpp::close::status::get_string(con->get_remote_close_code())
-    << "), close reason: " << con->get_remote_close_reason();
-    m_error_reason = s.str();
-    std::cerr << m_error_reason;
-}
-
-void ConnectionMetadata::on_message(websocketpp::connection_hdl hdl,
-                                    std::shared_ptr<websocketpp::message_buffer::message<websocketpp::message_buffer::alloc::con_msg_manager>> msg) {
-    std::stringstream s;
-
-    if (msg->get_opcode() == websocketpp::frame::opcode::text) {
-        s << msg->get_payload();
-    } else {
-        s << websocketpp::utility::to_hex(msg->get_payload());
-    }
-
-    for(int i = 0; i < message_listeners.size(); i++) {
-        message_listeners[i]->on_message(s.str());
-    }
-}
-
-websocketpp::connection_hdl ConnectionMetadata::get_hdl() const {
-    return m_hdl;
-}
-
-context_ptr PeerConnectionClient::on_tls_init(websocketpp::connection_hdl) {
-    context_ptr ctx = websocketpp::lib::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::sslv23);
-
-    auto options = boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::single_dh_use;
-    if (!verify_ssl) {
-        options = options | boost::asio::ssl::context::verify_none;
+        return new_id;
     }
 
-    try {
-        ctx->set_options(options);
-    } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
+    void PeerConnectionClient::close(int id, websocketpp::close::status::value code, std::string reason) {
+        websocketpp::lib::error_code ec;
+
+        con_list::iterator metadata_it = m_connection_list.find(id);
+        if (metadata_it == m_connection_list.end()) {
+            std::cerr << "> No connection found with id " << id << std::endl;
+            return;
+        }
+
+        m_endpoint.close(metadata_it->second->get_hdl(), code, reason, ec);
+        if (ec) {
+            std::cerr << "> Error initiating close: " << ec.message() << std::endl;
+        }
     }
 
-    return ctx;
-}
+    void ConnectionMetadata::on_open(ssl_client *c, websocketpp::connection_hdl hdl) {
+        m_status = OPEN;
 
-void PeerConnectionClient::send(int id, std::string message) {
-    websocketpp::lib::error_code ec;
-
-    con_list::iterator metadata_it = m_connection_list.find(id);
-    if (metadata_it == m_connection_list.end()) {
-        std::cout << "> No connection found with id " << id << std::endl;
-        return;
+        ssl_client::connection_ptr con = c->get_con_from_hdl(hdl);
+        m_server = con->get_response_header("Server");
     }
 
-    m_endpoint.send(metadata_it->second->get_hdl(), message, websocketpp::frame::opcode::text, ec);
-    if (ec) {
-        std::cout << "> Error sending message: " << ec.message() << std::endl;
-        return;
-    }
-}
+    void ConnectionMetadata::on_fail(ssl_client *c, websocketpp::connection_hdl hdl) {
+        m_status = FAILED;
 
-ConnectionMetadata::Status PeerConnectionClient::get_status(int id) {
-    con_list::iterator metadata_it = m_connection_list.find(id);
-    if (metadata_it == m_connection_list.end()) {
-        std::cout << "> No connection found with id " << id << std::endl;
-        return ConnectionMetadata::CLOSED;
+        ssl_client::connection_ptr con = c->get_con_from_hdl(hdl);
+        m_server = con->get_response_header("Server");
+        m_error_reason = con->get_ec().message();
+        std::cerr << "> Failed: " << m_error_reason << std::endl;
     }
 
-    return metadata_it->second->get_status();
-}
+    void ConnectionMetadata::on_close(ssl_client *c, websocketpp::connection_hdl hdl) {
+        m_status = CLOSED;
 
-void ConnectionMetadata::add_listener(PeerConnectionListener *listener) {
-    message_listeners.push_back(listener);
-}
-
-void PeerConnectionClient::add_listener(int id, PeerConnectionListener *listener) {
-    con_list::iterator metadata_it = m_connection_list.find(id);
-    if (metadata_it == m_connection_list.end()) {
-        std::cout << "> No connection found with id " << id << std::endl;
-        return;
+        ssl_client::connection_ptr con = c->get_con_from_hdl(hdl);
+        std::stringstream s;
+        s << "close code: " << con->get_remote_close_code() << " ("
+        << websocketpp::close::status::get_string(con->get_remote_close_code())
+        << "), close reason: " << con->get_remote_close_reason();
+        m_error_reason = s.str();
+        std::cerr << m_error_reason;
     }
 
-    metadata_it->second->add_listener(listener);
-}
+    void ConnectionMetadata::on_message(websocketpp::connection_hdl hdl,
+                                        std::shared_ptr<websocketpp::message_buffer::message<websocketpp::message_buffer::alloc::con_msg_manager>> msg) {
+        std::stringstream s;
+
+        if (msg->get_opcode() == websocketpp::frame::opcode::text) {
+            s << msg->get_payload();
+        } else {
+            s << websocketpp::utility::to_hex(msg->get_payload());
+        }
+
+        for (int i = 0; i < message_listeners.size(); i++) {
+            message_listeners[i]->on_message(s.str());
+        }
+    }
+
+    websocketpp::connection_hdl ConnectionMetadata::get_hdl() const {
+        return m_hdl;
+    }
+
+    context_ptr PeerConnectionClient::on_tls_init(websocketpp::connection_hdl) {
+        context_ptr ctx = websocketpp::lib::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::sslv23);
+
+        auto options = boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::single_dh_use;
+        if (!verify_ssl) {
+            options = options | boost::asio::ssl::context::verify_none;
+        }
+
+        try {
+            ctx->set_options(options);
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
+
+        return ctx;
+    }
+
+    void PeerConnectionClient::send(int id, std::string message) {
+        websocketpp::lib::error_code ec;
+
+        con_list::iterator metadata_it = m_connection_list.find(id);
+        if (metadata_it == m_connection_list.end()) {
+            std::cout << "> No connection found with id " << id << std::endl;
+            return;
+        }
+
+        m_endpoint.send(metadata_it->second->get_hdl(), message, websocketpp::frame::opcode::text, ec);
+        if (ec) {
+            std::cout << "> Error sending message: " << ec.message() << std::endl;
+            return;
+        }
+    }
+
+    ConnectionMetadata::Status PeerConnectionClient::get_status(int id) {
+        con_list::iterator metadata_it = m_connection_list.find(id);
+        if (metadata_it == m_connection_list.end()) {
+            std::cout << "> No connection found with id " << id << std::endl;
+            return ConnectionMetadata::CLOSED;
+        }
+
+        return metadata_it->second->get_status();
+    }
+
+    void ConnectionMetadata::add_listener(PeerConnectionListener *listener) {
+        message_listeners.push_back(listener);
+    }
+
+    void PeerConnectionClient::add_listener(int id, PeerConnectionListener *listener) {
+        con_list::iterator metadata_it = m_connection_list.find(id);
+        if (metadata_it == m_connection_list.end()) {
+            std::cout << "> No connection found with id " << id << std::endl;
+            return;
+        }
+
+        metadata_it->second->add_listener(listener);
+    }
+};
--- src/PeerConnectionClient.h
+++ src/PeerConnectionClient.h
@@ -8,86 +8,87 @@
 #include <websocketpp/config/asio_client.hpp>
 #include <websocketpp/client.hpp>
 
-typedef websocketpp::client<websocketpp::config::asio_tls_client> ssl_client;
-typedef websocketpp::lib::shared_ptr<boost::asio::ssl::context> context_ptr;
+namespace dynastat {
+    typedef websocketpp::client<websocketpp::config::asio_tls_client> ssl_client;
+    typedef websocketpp::lib::shared_ptr<boost::asio::ssl::context> context_ptr;
 
-class PeerConnectionListener {
-public:
-    virtual void on_message(std::string msg) = 0;
-};
-
-class ConnectionMetadata {
-public:
-    typedef websocketpp::lib::shared_ptr<ConnectionMetadata> ptr;
-
-    enum Status {
-        CONNECTING,
-        OPEN,
-        FAILED,
-        CLOSED
+    class PeerConnectionListener {
+    public:
+        virtual void on_message(std::string msg) = 0;
     };
 
-    ConnectionMetadata(int id, websocketpp::connection_hdl hdl, std::string uri) :
-            m_id(id),
-            m_hdl(hdl),
-            m_status(CONNECTING),
-            m_uri(uri) { };
+    class ConnectionMetadata {
+    public:
+        typedef websocketpp::lib::shared_ptr<ConnectionMetadata> ptr;
 
-    void on_open(ssl_client *c, websocketpp::connection_hdl hdl);
+        enum Status {
+            CONNECTING,
+            OPEN,
+            FAILED,
+            CLOSED
+        };
 
-    void on_fail(ssl_client *c, websocketpp::connection_hdl hdl);
+        ConnectionMetadata(int id, websocketpp::connection_hdl hdl, std::string uri) :
+                m_id(id),
+                m_hdl(hdl),
+                m_status(CONNECTING),
+                m_uri(uri) { };
 
-    void on_close(ssl_client *c, websocketpp::connection_hdl hdl);
+        void on_open(ssl_client *c, websocketpp::connection_hdl hdl);
 
-    void on_message(websocketpp::connection_hdl hdl, ssl_client::message_ptr msg);
+        void on_fail(ssl_client *c, websocketpp::connection_hdl hdl);
 
-    const Status &get_status() {
-        return m_status;
-    }
+        void on_close(ssl_client *c, websocketpp::connection_hdl hdl);
 
-    websocketpp::connection_hdl get_hdl() const;
+        void on_message(websocketpp::connection_hdl hdl, ssl_client::message_ptr msg);
 
-    void add_listener(PeerConnectionListener *listener);
+        const Status &get_status() {
+            return m_status;
+        }
 
-private:
-    int m_id;
-    websocketpp::connection_hdl m_hdl;
-    Status m_status;
-    std::string m_uri;
-    std::string m_server;
-    std::string m_error_reason;
+        websocketpp::connection_hdl get_hdl() const;
 
-    std::vector<PeerConnectionListener*> message_listeners;
+        void add_listener(PeerConnectionListener *listener);
+
+    private:
+        int m_id;
+        websocketpp::connection_hdl m_hdl;
+        Status m_status;
+        std::string m_uri;
+        std::string m_server;
+        std::string m_error_reason;
+
+        std::vector<PeerConnectionListener *> message_listeners;
+    };
+
+    class PeerConnectionClient {
+    public:
+        PeerConnectionClient();
+
+        ~PeerConnectionClient();
+
+        context_ptr on_tls_init(websocketpp::connection_hdl);
+
+        int connect(std::string const &uri);
+
+        void close(int id, websocketpp::close::status::value code, std::string reason);
+
+        void send(int id, std::string msg);
+
+        void add_listener(int id, PeerConnectionListener *listener);
+
+        ConnectionMetadata::Status get_status(int id);
+
+    private:
+        typedef std::map<int, ConnectionMetadata::ptr> con_list;
+
+        ssl_client m_endpoint;
+        websocketpp::lib::shared_ptr<websocketpp::lib::thread> m_thread;
+
+        con_list m_connection_list;
+        int m_next_id = 0;
+        const bool verify_ssl = false;
+    };
 };
-
-class PeerConnectionClient {
-public:
-    PeerConnectionClient();
-
-    ~PeerConnectionClient();
-
-    context_ptr on_tls_init(websocketpp::connection_hdl);
-
-    int connect(std::string const &uri);
-
-    void close(int id, websocketpp::close::status::value code, std::string reason);
-
-    void send(int id, std::string msg);
-
-    void add_listener(int id, PeerConnectionListener *listener);
-
-    ConnectionMetadata::Status get_status(int id);
-
-private:
-    typedef std::map<int, ConnectionMetadata::ptr> con_list;
-
-    ssl_client m_endpoint;
-    websocketpp::lib::shared_ptr<websocketpp::lib::thread> m_thread;
-
-    con_list m_connection_list;
-    int m_next_id = 0;
-    const bool verify_ssl = false;
-};
-
 
 #endif //CDYNASTAT_PEERCONNECTIONCLIENT_H
