96177b3cca26cac438ec2ee7fc5a65b4c1016436
Tom Price <tom@codedinternet.com>
2016-11-13 15:42:54 +0000

    Add hardware clock commands to startup script

--- start.sh
+++ start.sh
@@ -2,10 +2,15 @@
 
 # Enable i2c-1
 if ! [ -a /dev/i2c-1 ]; then
-    echo cape-universaln > /sys/devices/platform/bone_capemgr/slots && /app/src/cdynastat
+    echo "Enabling i2c-1" | tee -a /data/start.log
+    echo cape-universaln > /sys/devices/platform/bone_capemgr/slots
 fi
 
 # Setup local clock in case we are offline
+echo ds3231 0x68 >/sys/bus/i2c/devices/i2c-2/new_device
+sleep 1
+hwclock -f /dev/rtc1 -s
+
 echo "Started at " $(date) | tee -a /data/start.log
 
 # Start application
